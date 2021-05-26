f5039418cbf76aa0883693c40cf540f1881023fb
Simon Oxenford <oxenfordsimon@gmail.com>
2021-05-26 17:43:09 +0200

    set up metric and transform settings format

--- antsRegistration/antsRegistrationLib/util.py
+++ antsRegistration/antsRegistrationLib/util.py
@@ -16,80 +16,83 @@ class antsBase:
       if subcls.__name__ == name:
         return subcls()
 
+#
+# Metric
+#
+
 class antsMetric(antsBase):
   def __init__(self):
     super().__init__()
     
-
 class CC(antsMetric):
   def __init__(self):
     super().__init__()
     self.details = 'ANTS neighborhood cross correlation'
-    self.settingsFormat = 'metricWeight, radius, &lt;samplingStrategy={None,Regular,Random}&gt;, &lt;samplingPercentage=[0,1]&gt;'
-
+    self.settingsFormat = '<b>metricWeight</b>, <b>radius</b>, &lt;<b>samplingStrategy</b>={None,Regular,Random}&gt;, &lt;<b>samplingPercentage</b>=[0,1]&gt;'
+    self.settingsDefault = '1,4,Random,0.25'
 
 class MI(antsMetric):
   def __init__(self):
     super().__init__()
     self.details = 'Mutual Information'
-    self.settingsDefault = '1.25,32,Random,0.25'
-
+    self.settingsFormat = '<b>metricWeight</b>, <b>numberOfBins</b>, &lt;<b>samplingStrategy</b>={None,Regular,Random}&gt;, &lt;<b>samplingPercentage</b>=[0,1]&gt;'
+    self.settingsDefault = '1,32,Random,0.25'
 
 class Mattes(antsMetric):
   def __init__(self):
     super().__init__()
+    self.settingsFormat = '<b>metricWeight</b>, <b>numberOfBins</b>, &lt;<b>samplingStrategy</b>={None,Regular,Random}&gt;, &lt;<b>samplingPercentage</b>=[0,1]&gt;'
+    self.settingsDefault = '1,32,Random,0.25'
 
+class MeanSquares(antsMetric):
+  def __init__(self):
+    super().__init__()
+    self.settingsFormat = '<b>metricWeight</b>, <b>radius</b>=NA, &lt;<b>samplingStrategy</b>={None,Regular,Random}&gt;, &lt;<b>samplingPercentage</b>=[0,1]&gt;'
+    self.settingsDefault = '1,NA,Random,0.25'
 
+class Demons(antsMetric):
+  def __init__(self):
+    super().__init__()
+    self.settingsFormat = '<b>metricWeight</b>, <b>radius</b>=NA, &lt;<b>samplingStrategy</b>={None,Regular,Random}&gt;, &lt;<b>samplingPercentage</b>=[0,1]&gt;'
+    self.settingsDefault = '1,NA,Random,0.25'
 
+class GC(antsMetric):
+  def __init__(self):
+    super().__init__()
+    self.settingsFormat = '<b>metricWeight</b>, <b>radius</b>=NA, &lt;<b>samplingStrategy</b>={None,Regular,Random}&gt;, &lt;<b>samplingPercentage</b>=[0,1]&gt;'
+    self.settingsDefault = '1,NA,Random,0.25'
 
-MetricsNameInfo = {\
-  'CC': {\
-    'Details': 'ANTS neighborhood cross correlation',\
-    'Format': 'metricWeight, radius, &lt;samplingStrategy={None,Regular,Random}&gt;, &lt;samplingPercentage=[0,1]&gt;',\
-    'Default': ''},\
-  'MI': {\
-    'Details': 'Mutual Information',\
-    'Format': 'metricWeight, numberOfBins, &lt;samplingStrategy={None,Regular,Random}&gt;, &lt;samplingPercentage=[0,1]&gt;',\
-    'Default': '1.25,32,Random,0.25'},\
-  'Mattes': {\
-    'Details': '',\
-    'Format': 'metricWeight, numberOfBins, &lt;samplingStrategy={None,Regular,Random}&gt;, &lt;samplingPercentage=[0,1]&gt;',\
-    'Default': ''},\
-  'MeanSquares': {\
-    'Details': '',\
-    'Format': 'metricWeight, radius=NA, &lt;samplingStrategy={None,Regular,Random}&gt;, &lt;samplingPercentage=[0,1]&gt;',\
-    'Default': ''},\
-  'Demons': {\
-    'Details': '',\
-    'Format': 'metricWeight, radius=NA, &lt;samplingStrategy={None,Regular,Random}&gt;, &lt;samplingPercentage=[0,1]&gt;',\
-    'Default': ''},\
-  'GC': {\
-    'Details': 'Global Correlation',\
-    'Format': 'metricWeight, radius=NA, &lt;samplingStrategy={None,Regular,Random}&gt;, &lt;samplingPercentage=[0,1]&gt;',\
-    'Default': ''},\
-  'ICP': {\
-    'Details': 'Euclidean',\
-    'Format': 'metricWeight, &lt;samplingPercentage=[0,1]&gt;, &lt;boundaryPointsOnly=0&gt;',\
-    'Default': ''},\
-  'PSE': {\
-    'Details': 'Point-set expectation',\
-    'Format': 'metricWeight, &lt;samplingPercentage=[0,1]&gt;, &lt;boundaryPointsOnly=0&gt;,&lt;pointSetSigma=1&gt;, &lt;kNeighborhood=50&gt;',\
-    'Default': ''},\
-  'JHCT': {\
-    'Details': 'Jensen-Havrda-Charvet-Tsallis',\
-    'Format': 'metricWeight, &lt;samplingPercentage=[0,1]&gt;, &lt;boundaryPointsOnly=0&gt;, &lt;pointSetSigma=1&gt;, &lt;kNeighborhood=50&gt;, &lt;alpha=1.1&gt;, &lt;useAnisotropicCovariances=1&gt;',\
-    'Default': ''}\
-}
+class ICP(antsMetric):
+  def __init__(self):
+    super().__init__()
+    self.details = 'Euclidean'
+    self.settingsFormat = '<b>metricWeight</b>, &lt;<b>samplingPercentage</b>=[0,1]&gt;, &lt;<b>boundaryPointsOnly</b>=0&gt;'
+    self.settingsDefault = '1,0.25,0'
 
+class PSE(antsMetric):
+  def __init__(self):
+    super().__init__()
+    self.details = 'Point-set expectation'
+    self.settingsFormat = '<b>metricWeight</b>, &lt;<b>samplingPercentage</b>=[0,1]&gt;, &lt;<b>boundaryPointsOnly</b>=0&gt;,&lt;<b>pointSetSigma</b>=1&gt;, &lt;<b>kNeighborhood</b>=50&gt;'
+    self.settingsDefault = '1,0.25,0,1,50'
 
+class JHCT(antsMetric):
+  def __init__(self):
+    super().__init__()
+    self.details = 'Jensen-Havrda-Charvet-Tsallis'
+    self.settingsFormat = '<b>metricWeight</b>, &lt;<b>samplingPercentage</b>=[0,1]&gt;, &lt;<b>boundaryPointsOnly</b>=0&gt;, &lt;<b>pointSetSigma</b>=1&gt;, &lt;<b>kNeighborhood</b>=50&gt;, &lt;<b>alpha</b>=1.1&gt;, &lt;<b>useAnisotropicCovariances</b>=1&gt;'
+    self.settingsDefault = '1,0.25,0,1,50,1.1,1'
 
-
+#
+# Transforms
+#
 
 class antsTransform(antsBase):
   def __init__(self):
     super().__init__()
+    self.details = ''
+    self.settingsFormat = '<b>gradientStep</b>'
     self.settingsDefault = '0.1'
-
 
 class Rigid(antsTransform):
   def __init__(self):
@@ -99,70 +102,68 @@ class Affine(antsTransform):
   def __init__(self):
     super().__init__()
 
+class CompositeAffine(antsTransform):
+  def __init__(self):
+    super().__init__()
 
+class Similarity(antsTransform):
+  def __init__(self):
+    super().__init__()
 
+class Translation(antsTransform):
+  def __init__(self):
+    super().__init__()
 
+class BSpline(antsTransform):
+  def __init__(self):
+    super().__init__()
+    self.settingsFormat = '<b>gradientStep</b>, <b>meshSizeAtBaseLevel</b>'
+    self.settingsDefault = '0.1,8'
 
+class GaussianDisplacementField(antsTransform):
+  def __init__(self):
+    super().__init__()
+    self.settingsFormat = '<b>gradientStep</b>, <b>updateFieldVarianceInVoxelSpace</b>, <b>totalFieldVarianceInVoxelSpace</b>'
+    self.settingsDefault = '0.1,3,0'
 
+class BSplineDisplacementField(antsTransform):
+  def __init__(self):
+    super().__init__()
+    self.settingsFormat = '<b>gradientStep</b>, <b>updateFieldMeshSizeAtBaseLevel</b>, &lt;<b>totalFieldMeshSizeAtBaseLevel</b>=0&gt;, &lt;<b>splineOrder</b>=3&gt;'
+    self.settingsDefault = '0.1,26,0,3'
 
+class TimeVaryingVelocityField(antsTransform):
+  def __init__(self):
+    super().__init__()
+    self.settingsFormat = '<b>gradientStep</b>, <b>numberOfTimeIndices</b>, <b>updateFieldVarianceInVoxelSpace</b>, <b>updateFieldTimeVariance</b>, <b>totalFieldVarianceInVoxelSpace</b>, <b>totalFieldTimeVariance</b>'
+    self.settingsDefault = ''
 
+class TimeVaryingBSplineVelocityField(antsTransform):
+  def __init__(self):
+    super().__init__()
+    self.settingsFormat = '<b>gradientStep</b>, <b>velocityFieldMeshSize</b>, &lt;<b>numberOfTimePointSamples</b>=4&gt;, &lt;<b>splineOrder</b>=3&gt;'
+    self.settingsDefault = ''
 
+class SyN(antsTransform):
+  def __init__(self):
+    super().__init__()
+    self.settingsFormat = '<b>gradientStep</b>, &lt;<b>updateFieldVarianceInVoxelSpace</b>= 3&gt;, &lt;<b>totalFieldVarianceInVoxelSpace</b>=0&gt;'
+    self.settingsDefault = '0.1,3,0'
 
-TransformsNameInfo = {\
-  'Rigid': {\
-    'Details': '',\
-    'Format': 'gradientStep',\
-    'Default': '0.1'},\
-  'Affine': {\
-    'Details': '',\
-    'Format': 'gradientStep',\
-    'Default': ''},\
-  'CompositeAffine': {\
-    'Details': '',\
-    'Format': 'gradientStep',\
-    'Default': ''},\
-  'Similarity': {\
-    'Details': '',\
-    'Format': 'gradientStep',\
-    'Default': ''},\
-  'Translation': {\
-    'Details': '',\
-    'Format': 'gradientStep',\
-    'Default': ''},\
-  'BSpline': {\
-    'Details': '',\
-    'Format': 'gradientStep, meshSizeAtBaseLevel',\
-    'Default': ''},\
-  'GaussianDisplacementField': {\
-    'Details': '',\
-    'Format': 'gradientStep, updateFieldVarianceInVoxelSpace, totalFieldVarianceInVoxelSpace',\
-    'Default': ''},\
-  'BSplineDisplacementField': {\
-    'Details': '',\
-    'Format': 'gradientStep, updateFieldMeshSizeAtBaseLevel, &lt;totalFieldMeshSizeAtBaseLevel=0&gt;, &lt;splineOrder=3&gt;',\
-    'Default': ''},\
-  'TimeVaryingVelocityField': {\
-    'Details': '',\
-    'Format': 'gradientStep, numberOfTimeIndices, updateFieldVarianceInVoxelSpace, updateFieldTimeVariance, totalFieldVarianceInVoxelSpace, totalFieldTimeVariance',\
-    'Default': ''},\
-  'TimeVaryingBSplineVelocityField': {\
-    'Details': '',\
-    'Format': 'gradientStep, velocityFieldMeshSize, &lt;numberOfTimePointSamples=4&gt;, &lt;splineOrder=3&gt;',\
-    'Default': ''},\
-  'SyN': {\
-    'Details': '',\
-    'Format': 'gradientStep, &lt;updateFieldVarianceInVoxelSpace= 3&gt;, &lt;totalFieldVarianceInVoxelSpace=0&gt;',\
-    'Default': ''},\
-  'BSplineSyN': {\
-    'Details': '',\
-    'Format': 'gradientStep, updateFieldMeshSizeAtBaseLevel, &lt;totalFieldMeshSizeAtBaseLevel=0&gt;, &lt;splineOrder=3&gt;',\
-    'Default': ''},\
-  'Exponential': {\
-    'Details': '',\
-    'Format': 'gradientStep, updateFieldVarianceInVoxelSpace, velocityFieldVarianceInVoxelSpace, &lt;numberOfIntegrationSteps&gt;',\
-    'Default': ''},\
-  'BSplineExponential': {\
-    'Details': '',\
-    'Format': 'gradientStep, updateFieldMeshSizeAtBaseLevel, &lt;velocityFieldMeshSizeAtBaseLevel=0&gt;, &lt;numberOfIntegrationSteps&gt;, &lt;splineOrder=3&gt;',\
-    'Default': ''}\
-}
+class BSplineSyN(antsTransform):
+  def __init__(self):
+    super().__init__()
+    self.settingsFormat = '<b>gradientStep</b>, <b>updateFieldMeshSizeAtBaseLevel</b>, &lt;<b>totalFieldMeshSizeAtBaseLevel</b>=0&gt;, &lt;<b>splineOrder</b>=3&gt;'
+    self.settingsDefault = '0.1,26,0,3'
+
+class Exponential(antsTransform):
+  def __init__(self):
+    super().__init__()
+    self.settingsFormat = '<b>gradientStep</b>, <b>updateFieldVarianceInVoxelSpace</b>, <b>velocityFieldVarianceInVoxelSpace</b>, &lt;<b>numberOfIntegrationSteps</b>&gt;'
+    self.settingsDefault = ''
+
+class Exponential(antsTransform):
+  def __init__(self):
+    super().__init__()
+    self.settingsFormat = '<b>gradientStep</b>, <b>updateFieldMeshSizeAtBaseLevel</b>, &lt;<b>velocityFieldMeshSizeAtBaseLevel</b>=0&gt;, &lt;<b>numberOfIntegrationSteps</b>&gt;, &lt;<b>splineOrder</b>=3&gt;'
+    self.settingsDefault = ''
