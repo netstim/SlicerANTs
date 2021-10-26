494f0a2df44dde47401c7b3903a75c7ffffc4b89
Simon Oxenford <oxenfordsimon@gmail.com>
2021-10-26 17:44:16 -0300

    COMP: init ants build with libs for CLI module

--- SuperBuild/ants_patch.cmake
+++ SuperBuild/ants_patch.cmake
@@ -5,6 +5,8 @@ set(build_ants_apps
   antsApplyTransforms
 )
 
+file(COPY ${CMAKE_CURRENT_LIST_DIR}/antsConfig.cmake DESTINATION ${ants_SRC_DIR}/Examples/)
+
 set(cmakefile
   ${ants_SRC_DIR}/Examples/CMakeLists.txt
 )
@@ -17,6 +19,48 @@ string(REGEX REPLACE "\\(CORE_ANTS_APPS[^\\)]+" "\(CORE_ANTS_APPS ${build_ants_a
     cmakefile_src "${cmakefile_src}")
 string(REGEX REPLACE "\\(MORE_ANTS_APPS[^\\)]+" "\(MORE_ANTS_APPS "
     cmakefile_src "${cmakefile_src}")
+string(REPLACE "install(TARGETS antsUtilities" "install(TARGETS antsUtilities EXPORT antsTargets"
+    cmakefile_src "${cmakefile_src}")
+string(REPLACE "install(TARGETS l_\${ANTS_FUNCTION_NAME} \${ANTS_FUNCTION_NAME}" "install(TARGETS l_\${ANTS_FUNCTION_NAME} \${ANTS_FUNCTION_NAME} EXPORT antsTargets"
+    cmakefile_src "${cmakefile_src}")
+string(APPEND cmakefile_src
+"
+include(CMakePackageConfigHelpers)
+write_basic_package_version_file(
+  \"\${CMAKE_CURRENT_BINARY_DIR}/../antsConfigVersion.cmake\"
+  VERSION 0.1
+  COMPATIBILITY AnyNewerVersion
+)
+
+export(EXPORT antsTargets
+  FILE \"\${CMAKE_CURRENT_BINARY_DIR}/../antsTargets.cmake\"
+  NAMESPACE Upstream::
+)
+configure_file(antsConfig.cmake
+  \"\${CMAKE_CURRENT_BINARY_DIR}/../antsConfig.cmake\"
+  COPYONLY
+)
+
+set(ConfigPackageLocation \${CMAKE_CURRENT_BINARY_DIR}/../ants)
+install(EXPORT antsTargets
+  FILE
+    antsTargets.cmake
+  NAMESPACE
+    Upstream::
+  DESTINATION
+    \${ConfigPackageLocation}
+)
+install(
+  FILES
+    antsConfig.cmake
+    \"\${CMAKE_CURRENT_BINARY_DIR}/../antsConfigVersion.cmake\"
+  DESTINATION
+    \${ConfigPackageLocation}
+  COMPONENT
+    Devel
+)
+"
+)
 file(WRITE ${cmakefile} "${cmakefile_src}")
 else()
 message(STATUS "ants: Already patched ${cmakefile}")
