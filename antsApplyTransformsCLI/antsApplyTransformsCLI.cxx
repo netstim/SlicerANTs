8988eec992da013e77facf958a6d256cb29188bd
Simon Oxenford <oxenfordsimon@gmail.com>
2021-10-28 11:50:48 -0300

    ENH: add ants apply transforms cli functionality

--- antsApplyTransformsCLI/antsApplyTransformsCLI.cxx
+++ antsApplyTransformsCLI/antsApplyTransformsCLI.cxx
@@ -1,9 +1,3 @@
-#include "itkImageFileWriter.h"
-
-#include "itkSmoothingRecursiveGaussianImageFilter.h"
-
-#include "itkPluginUtilities.h"
-
 #include "antsApplyTransformsCLICLP.h"
 
 #include "antsApplyTransforms.h"
@@ -12,119 +6,65 @@
 #include <vector>
 #include <string>
 
-// Use an anonymous namespace to keep class types and function names
-// from colliding when module is used as shared object module.  Every
-// thing should be in an anonymous namespace except for the module
-// entry point, e.g. main()
-//
 namespace
 {
-
-// template <typename TPixel>
-int DoIt( int argc, char * argv[])
-{
-  PARSE_ARGS;
-
-  const char * const * const ptr = &argv[1] ;
-  const std::vector< std::string > command_line_args( ptr , ptr+argc-1 );
-  std::ostream * command_stream = &std::cout;
-  return ants::antsApplyTransforms( command_line_args , command_stream ) ;
-
-
-  // typedef TPixel InputPixelType;
-  // typedef TPixel OutputPixelType;
-
-  // const unsigned int Dimension = 3;
-
-  // typedef itk::Image<InputPixelType,  Dimension> InputImageType;
-  // typedef itk::Image<OutputPixelType, Dimension> OutputImageType;
-
-  // typedef itk::ImageFileReader<InputImageType>  ReaderType;
-
-  // typename ReaderType::Pointer reader = ReaderType::New();
-
-  // reader->SetFileName( inputVolume.c_str() );
-
-  // typedef itk::SmoothingRecursiveGaussianImageFilter<
-  //   InputImageType, OutputImageType>  FilterType;
-  // typename FilterType::Pointer filter = FilterType::New();
-  // filter->SetInput( reader->GetOutput() );
-  // filter->SetSigma( sigma );
-
-  // typedef itk::ImageFileWriter<OutputImageType> WriterType;
-  // typename WriterType::Pointer writer = WriterType::New();
-  // writer->SetFileName( outputVolume.c_str() );
-  // writer->SetInput( filter->GetOutput() );
-  // writer->SetUseCompression(1);
-  // writer->Update();
-
-  return EXIT_SUCCESS;
+  void printAntsApplyTransformsCommand(std::vector< std::string > * command_line_args)
+  {
+    std::cout << "antsApplyTransforms ";
+    for(const auto& cla: *command_line_args) {
+      std::cout << cla << " ";
+    }
+    std::cout << std::endl;
+  }
 }
-
-} // end of anonymous namespace
 
 int main( int argc, char * argv[] )
 {
   PARSE_ARGS;
 
-  return DoIt( argc, argv);
+  if (referenceVolume.empty())
+  {
+    std::cout << "ERROR: missing reference volume" << std::endl;
+    return EXIT_FAILURE;
+  }
 
-  // itk::ImageIOBase::IOPixelType     pixelType;
-  // itk::ImageIOBase::IOComponentType componentType;
+  if (transform.empty() && transformFile.empty())
+  {
+    std::cout << "ERROR: missing transform" << std::endl;
+    return EXIT_FAILURE;
+  }
 
-  // try
-  //   {
-  //   itk::GetImageType(inputVolume, pixelType, componentType);
+  if (!transform.empty() && !transformFile.empty())
+  {
+    std::cout << "ERROR: specify either a transform node or a transform file." << std::endl;
+    return EXIT_FAILURE;
+  }
 
-  //   // This filter handles all types on input, but only produces
-  //   // signed types
-  //   switch( componentType )
-  //     {
-  //     case itk::ImageIOBase::UCHAR:
-  //       return DoIt( argc, argv, static_cast<unsigned char>(0) );
-  //       break;
-  //     case itk::ImageIOBase::CHAR:
-  //       return DoIt( argc, argv, static_cast<signed char>(0) );
-  //       break;
-  //     case itk::ImageIOBase::USHORT:
-  //       return DoIt( argc, argv, static_cast<unsigned short>(0) );
-  //       break;
-  //     case itk::ImageIOBase::SHORT:
-  //       return DoIt( argc, argv, static_cast<short>(0) );
-  //       break;
-  //     case itk::ImageIOBase::UINT:
-  //       return DoIt( argc, argv, static_cast<unsigned int>(0) );
-  //       break;
-  //     case itk::ImageIOBase::INT:
-  //       return DoIt( argc, argv, static_cast<int>(0) );
-  //       break;
-  //     case itk::ImageIOBase::ULONG:
-  //       return DoIt( argc, argv, static_cast<unsigned long>(0) );
-  //       break;
-  //     case itk::ImageIOBase::LONG:
-  //       return DoIt( argc, argv, static_cast<long>(0) );
-  //       break;
-  //     case itk::ImageIOBase::FLOAT:
-  //       return DoIt( argc, argv, static_cast<float>(0) );
-  //       break;
-  //     case itk::ImageIOBase::DOUBLE:
-  //       return DoIt( argc, argv, static_cast<double>(0) );
-  //       break;
-  //     case itk::ImageIOBase::UNKNOWNCOMPONENTTYPE:
-  //     default:
-  //       std::cerr << "Unknown input image pixel component type: ";
-  //       std::cerr << itk::ImageIOBase::GetComponentTypeAsString( componentType );
-  //       std::cerr << std::endl;
-  //       return EXIT_FAILURE;
-  //       break;
-  //     }
-  //   }
+  if (!outputDisplacementField.empty() && !outputVolume.empty())
+  {
+    std::cout << "ERROR: specify either an output volume or an output transform (not both)" << std::endl;
+    return EXIT_FAILURE;
+  }
 
-  // catch( itk::ExceptionObject & excep )
-  //   {
-  //   std::cerr << argv[0] << ": exception caught !" << std::endl;
-  //   std::cerr << excep << std::endl;
-  //   return EXIT_FAILURE;
-  //   }
-  // return EXIT_SUCCESS;
+  const char * const * const ptr = &argv[1] ;
+  std::vector< std::string > command_line_args( ptr , ptr+argc-1 );
+
+  for(std::size_t i = 0; i < command_line_args.size(); i=i+2) {
+    if (command_line_args[i].compare("--outputDisplacementField") == 0){
+      command_line_args[i] = "--output";
+      command_line_args[i+1] = "[" + command_line_args[i+1] + ",1]";
+    }
+    else if (command_line_args[i].compare("--transformFile") == 0){
+      command_line_args[i] = "--transform";
+    }
+  }
+
+  command_line_args.push_back("--v");
+  command_line_args.push_back("1");
+
+  printAntsApplyTransformsCommand(&command_line_args);
+
+  std::ostream * command_stream = &std::cout;
+
+  return ants::antsApplyTransforms( command_line_args , command_stream ) ;
 }
